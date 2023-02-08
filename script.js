4c5af1fa0ab1c646f8f88f4ad1ff3cf3a2406927
Hannah Karlsson <gamemod.mod@gmail.com>
2023-02-08 15:00:23 +0100

    jpg => webp

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 const header = document.getElementById("imgHeader");
 let images = [
-  ["img/img1.jpg", "Image 1"],
-  ["img/img1.jpg", "Image 2"],
-  ["img/img1.jpg", "Image 3"],
-  ["img/img1.jpg", "Image 4"],
+  ["img/img1.webp", "Boka tid"],
+  ["img/img2.webp", "Image 2"],
+  ["img/img3.webp", "Image 3"],
+  ["img/img4.webp", "Färga håret?"],
 ];
 let index = 0;
 
 function imgHeaderLoop() {
+  console.log(index);
   //Kolla om vi är större än längden av images
-  if (index > images.length || index < 0) {
+  if (index >= images.length || index < 0) {
     index = 0;
   }
   //Hämta bilden från index.
+  console.log(images[index][0]);
   header.style.backgroundImage = `url(${images[index][0]})`;
   for (const child of header.children) {
     if (child.tagName === "H1") {
@@ -35,8 +37,9 @@ function fixImages(url) {
   */
   if (
     !(
-      window.location.pathname.includes("contact") ||
-      window.location.pathname.includes("bokning")
+      window.location.pathname.includes("about") ||
+      window.location.pathname.includes("bokning") ||
+      window.location.pathname.includes("contact")
     )
   ) {
     return url;
@@ -51,3 +54,63 @@ for (let i = 0; i < images.length; i++) {
 }
 console.log(images);
 imgHeaderLoop();
+
+/*
+Kundvagn
+*/
+const cartElement = document.querySelector("#cart");
+class cartItem {
+  constructor(id, name, cost) {
+    this.id = id;
+    this.name = name;
+    this.cost = cost;
+  }
+
+  ToString() {
+    return `${this.name} - ${this.cost}`;
+  }
+}
+class cart {
+  constructor(id, name, items) {
+    this.id = id;
+    this.name = name;
+    this.items = items;
+  }
+
+  AddToCart(item) {
+    this.items.push(item);
+  }
+
+  RemoveFromCart(item) {
+    if (item in this.items) {
+      this.items.pop();
+    }
+    console.log(this.items);
+    this.CreateCartAsElements();
+  }
+
+  ReturnCart() {
+    return this.items;
+  }
+
+  CreateCartAsElements() {
+    cartElement.innerHTML = ``;
+    if (this.items.length > 0) {
+      for (const item of this.items) {
+        const a = document.createElement("li");
+        a.innerText = `${item.ToString()}`;
+        a.classList.add("dropdown-item");
+        a.onclick = () => {
+          this.RemoveFromCart(item);
+        };
+        cartElement.appendChild(a);
+      }
+    } else {
+      console.log(this.name);
+    }
+  }
+}
+const newCart = new cart(0, "test", []);
+newCart.AddToCart(new cartItem(0, "test", 5));
+newCart.AddToCart(new cartItem(0, "Test 2", 15));
+newCart.CreateCartAsElements();
